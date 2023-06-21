037181c22c6fdf038cbb492b164c6440639e7748
Evans <ngugie649@gmail.com>
2023-06-21 09:08:40 +0300

    several changes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,14 +3,15 @@ import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/im3.jpg'
 import HeaderSocials from './headerSocials'
-
+import {useTyping} from 'use-typing-hook'
 const header = () => {
+  const typingText = useTyping("react and react Native developer", 200, 400)
   return (
     <header>
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Evans Ngugi</h1>
-        <h5 className="text-light">react and react Native developer</h5>
+        <h5 className="text-light">{typingText}</h5>
         <CTA/>
         <HeaderSocials/>
         <div className="me">
