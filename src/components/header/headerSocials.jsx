741823c39a062cb70e055fd77e324631e51e05b5
Evans <ngugie649@gmail.com>
2023-07-13 19:10:38 +0300

    links

--- src/components/header/headerSocials.jsx
+++ src/components/header/headerSocials.jsx
@@ -7,8 +7,8 @@ import {ImYoutube2} from 'react-icons/im'
 const headerSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https:/linkedin.com" target='_blank' id='icon' rel="noreferrer"><BsLinkedin/></a>
-        <a href="https:/Github.com" target='_blank' rel="noreferrer"><BsGithub/></a>
+        <a href="www.linkedin.com/in/evans-ngugi-719006232" target='_blank' id='icon' rel="noreferrer"><BsLinkedin/></a>
+        <a href="https://github.com/Evanoken" target='_blank' rel="noreferrer"><BsGithub/></a>
         <a href="https:/facebook.com" target='_blank' rel="noreferrer"><BsFacebook/></a>
         <a href="https:/youtube.com" target='_blank' rel="noreferrer"><ImYoutube2/></a>
     </div>
